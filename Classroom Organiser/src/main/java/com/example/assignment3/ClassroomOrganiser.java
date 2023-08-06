83052a6d6c722593149dc0b4266e5e1fe934c983
Emily Rose <122296201+roseemi@users.noreply.github.com>
2023-08-05 20:52:24 -0400

    Add files via upload

---
+++ Classroom Organiser/src/main/java/com/example/assignment3/ClassroomOrganiser.java
@@ -0,0 +1,24 @@
+package com.example.assignment3;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ClassroomOrganiser extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(ClassroomOrganiser.class.getResource("seating-plan.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Class Organiser");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
