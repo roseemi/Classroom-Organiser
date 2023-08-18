b9c4c4b029e6f189dfa6cee94b7984a2f2dc96dc
Emily Rose <122296201+roseemi@users.noreply.github.com>
2023-08-18 04:56:16 -0400

    Delete Classroom Organiser directory

--- Classroom Organiser/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.assignment3 {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens com.example.assignment3 to javafx.fxml;
-    exports com.example.assignment3;
-}
